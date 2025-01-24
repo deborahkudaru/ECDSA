e72e0a741bff7906c93605d4f2e4899ffdcd902d
Kudaru Deborah <deborahkudaru@gmail.com>
2025-01-24 01:55:35 -0800

    better

--- ecdsa.js
+++ ecdsa.js
@@ -1,41 +1,15 @@
-// generate public and private key, and address using ecdsa 
-
-// STEP 1
-// let keyPair = await window.crypto.subtle.generateKey(
-//     {
-//         name: "RSA-OAEP",
-//         modulusLength: 4096,
-//         publicExponent: new Uint8Array([1, 0, 1]),
-//         hash: "SHA-256"
-//     },
-//     true,
-//     ["encrypt", "decrypt"],
-// );
-// const privateKey = keyPair.privateKey;
-
-// console.log(privateKey)
-
-// function createAddress(gPoint, PRK) {
-//     // let generatorPoint = ["x", "y"]
-//     let publicKey = gPoint * PRK;
-//     let bytes20 = publicKey.slice(20);
-//     let address = `0x${bytes20}`;
-//     return address;
-// }
-
-
-
-// console.log(createAddress(gPoint, privateKey))
-
-// console.log()
+const keccak256 = require('keccak256')
 
 let prk = Math.floor(Math.random() * (1.158 * 10 ** 77));
 let prkToHex =  Math.abs(prk).toString(16);
 let gPoint =10
 let publicKey = Math.abs(prk * gPoint).toString(16)
+let hashofPublicKey = keccak256(Buffer.from(publicKey))
 
 function createAddress(){
-    return `0x${publicKey.slice(45)}`
+    return `address: 0x${hashofPublicKey.toString('hex').slice(20)}`
 }
+console.log(`private key: ${prkToHex}`)
+console.log(`public key: ${hashofPublicKey.toString('hex')}`)
 
 console.log(createAddress())
---
+++ ggg.js
@@ -0,0 +1,30 @@
+// generate public and private key, and address using ecdsa 
+
+// STEP 1
+// let keyPair = await window.crypto.subtle.generateKey(
+//     {
+//         name: "RSA-OAEP",
+//         modulusLength: 4096,
+//         publicExponent: new Uint8Array([1, 0, 1]),
+//         hash: "SHA-256"
+//     },
+//     true,
+//     ["encrypt", "decrypt"],
+// );
+// const privateKey = keyPair.privateKey;
+
+// console.log(privateKey)
+
+// function createAddress(gPoint, PRK) {
+//     // let generatorPoint = ["x", "y"]
+//     let publicKey = gPoint * PRK;
+//     let bytes20 = publicKey.slice(20);
+//     let address = `0x${bytes20}`;
+//     return address;
+// }
+
+
+
+// console.log(createAddress(gPoint, privateKey))
+
+// console.log()
