583377fa3585e0e2a59cc607cd8392ea0c0b8879
Kudaru Deborah <deborahkudaru@gmail.com>
2025-01-24 02:07:24 -0800

    naming convention

--- ecdsa.js
+++ ecdsa.js
@@ -1,15 +1,15 @@
 const keccak256 = require('keccak256')
 
-let prk = Math.floor(Math.random() * (1.158 * 10 ** 77));
-let prkToHex =  Math.abs(prk).toString(16);
+let privateKey = Math.floor(Math.random() * (2**256));
+let privateKeyToHex =  Math.abs(privateKey).toString(16);
 let gPoint =10
-let publicKey = Math.abs(prk * gPoint).toString(16)
+let publicKey = Math.abs(privateKey * gPoint).toString(16)
 let hashofPublicKey = keccak256(Buffer.from(publicKey))
 
 function createAddress(){
     return `address: 0x${hashofPublicKey.toString('hex').slice(20)}`
 }
-console.log(`private key: ${prkToHex}`)
+console.log(`private key: ${privateKeyToHex}`)
 console.log(`public key: ${hashofPublicKey.toString('hex')}`)
 
 console.log(createAddress())
