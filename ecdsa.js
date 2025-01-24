8d3732cb3993b163c6d7a16fca617d67cd9ec70d
Kudaru Deborah <deborahkudaru@gmail.com>
2025-01-23 22:04:44 -0800

    create address from public and private key

--- ecdsa.js
+++ ecdsa.js
@@ -1,6 +1,6 @@
 // generate public and private key, and address using ecdsa 
 
-
+// STEP 1
 let keyPair = await window.crypto.subtle.generateKey(
     {
         name: "RSA-OAEP",
@@ -15,14 +15,21 @@ const privateKey = keyPair.privateKey;
 
 console.log(privateKey)
 
-function createAddress(gPoint, PRK) {
-    // let generatorPoint = ["x", "y"]
-    let publicKey = gPoint * PRK;
-    let bytes20 = publicKey.slice(20);
-    let address = `0x${bytes20}`;
-    return address;
+// function createAddress(gPoint, PRK) {
+//     // let generatorPoint = ["x", "y"]
+//     let publicKey = gPoint * PRK;
+//     let bytes20 = publicKey.slice(20);
+//     let address = `0x${bytes20}`;
+//     return address;
+// }
+
+
+
+// console.log(createAddress(gPoint, privateKey))
+
+// console.log()
+
+
+function createPrivateKey(){
+    let prk = Math.floor(Math.random() * 10)
 }
-
-console.log(createAddress(10, privateKey))
-
-
